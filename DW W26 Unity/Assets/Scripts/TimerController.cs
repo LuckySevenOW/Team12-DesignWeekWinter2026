1b0f10384b2d0bea38c244bf72417b0b5d253527
MichaelG-design <000952746@mohawkcollege.ca>
2026-02-11 22:13:46 -0500

    Tie Screen
    - Set up Win/Lose/Tie panels
    - Made the draw panel appear after the 5 minute timer
    - Added a Quit button for each draw screen
    -Both team has the base placeholders in
    - Moved all player spawns
    - Fixed up invisble barriers

--- DW W26 Unity/Assets/Scripts/TimerController.cs
+++ DW W26 Unity/Assets/Scripts/TimerController.cs
@@ -1,14 +1,22 @@
-using UnityEngine;
 using TMPro;
+using Unity.VisualScripting.Antlr3.Runtime.Collections;
+using UnityEngine;
 
 public class TimerController : MonoBehaviour
 {
     //This equals 5 minutes, we can easily change it in the inspector on the timer manager btw
     public float time = 300f;
-    public TextMeshProUGUI Countdown;
+    public TextMeshProUGUI Countdown1;
+    public TextMeshProUGUI Countdown2;
 
     //This is for the 5 second countdown, its to delay my 5 minute game timer
     public float startDelay = 5f;
+
+    //This is for the panel gameobjects that will be the draw screens
+    public GameObject DrawScreenDisplay1;
+    public GameObject DrawScreenDisplay2;
+
+    private bool hasDrawed = false;
 
     void Update()
     {
@@ -23,6 +31,19 @@ public class TimerController : MonoBehaviour
         time -= Time.deltaTime;
         if (time < 0) time = 0;
 
-        Countdown.text = ((int)time / 60) + ":" + ((int)time % 60).ToString("00");
+        Countdown1.text = ((int)time / 60) + ":" + ((int)time % 60).ToString("00");
+        Countdown2.text = ((int)time / 60) + ":" + ((int)time % 60).ToString("00");
+
+        //Activate the draw screens on both displays
+        if (time <= 0f && !hasDrawed)
+        {
+            hasDrawed = true;
+
+            if (DrawScreenDisplay1 != null)
+                DrawScreenDisplay1.SetActive(true);
+
+            if (DrawScreenDisplay2 != null)
+                DrawScreenDisplay2.SetActive(true);
+        }
     }
 }
