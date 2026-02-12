8db337d9c47832b9ddace33b32c92082bfe74771
GeorgeGimme <144726058+imasleeping@users.noreply.github.com>
2026-02-12 03:58:51 -0500

    Started adding special Cannon

--- DW W26 Unity/Assets/Scripts/BulletDamage.cs
+++ DW W26 Unity/Assets/Scripts/BulletDamage.cs
@@ -5,7 +5,13 @@ public class BulletDamage : MonoBehaviour
     Rigidbody2D CurrCollider;
     LayerMask destroyablelayers;
     public GameObject itself;
-    int BulDam;
+    public bool Explodes;
+    public Rigidbody2D[] ExplosionTargets;
+    public int BulDam;
+    public int ExplosiveDam;
+
+    public int BulletKnockback;
+    public int ExplosiveKnockback;
     [field: SerializeField] public Rigidbody2D Rigidbody2D { get; private set; }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void setdamage(int damage)
@@ -22,7 +28,9 @@ public class BulletDamage : MonoBehaviour
             if (CurrCollider)
           {
             if (CurrCollider.transform.GetComponent<Destructable>())
-                CurrCollider.transform.GetComponent<Destructable>().Hit(BulDam);
+                {
+                    CurrCollider.transform.GetComponent<Destructable>().Hit(BulDam); 
+                }
             else
             {
                 if (CurrCollider.transform.GetComponent<WinTarget>())
@@ -34,13 +42,23 @@ public class BulletDamage : MonoBehaviour
         }
         else if (collision.gameObject.CompareTag("player"))
         {
-            collision.transform.GetComponent<Rigidbody2D>().linearVelocity = itself.transform.position - collision.transform.position;
+            collision.transform.GetComponent<Rigidbody2D>().linearVelocity = (itself.transform.position - collision.transform.position) * BulletKnockback;
         }
-            Destroy(itself, 0.01f);
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (Explodes)
+        {
+            foreach (Rigidbody2D Target in ExplosionTargets)
+            {
+                if (Target.transform.GetComponent<Destructable>())
+                {
+                    Target.transform.GetComponent<Destructable>().Hit(ExplosiveDam);
+                }
+                else if (collision.gameObject.CompareTag("player"))
+                {
+                    collision.transform.GetComponent<Rigidbody2D>().linearVelocity = (itself.transform.position - collision.transform.position) * ExplosiveKnockback;
+                }
+
+            }
+        }
+        Destroy(itself, 0.01f);
     }
 }
