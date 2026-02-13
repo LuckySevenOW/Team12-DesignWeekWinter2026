3ba0283a3f5a2b289d8171e7ab235b9e8fcefe0b
GeorgeGimme <144726058+imasleeping@users.noreply.github.com>
2026-02-12 22:53:32 -0500

    cannons drop when there is no floor under them

--- DW W26 Unity/Assets/CannonPlayerCheck.cs
+++ DW W26 Unity/Assets/CannonPlayerCheck.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class CannonPlayerCheck : MonoBehaviour
 {
+    bool movedown;
     public Rigidbody2D Cannon;
     private void OnTriggerEnter2D(Collider2D collider)
     {
@@ -9,6 +10,10 @@ public class CannonPlayerCheck : MonoBehaviour
         {
             collider.transform.GetComponent<PlayerController>().TouchingCannon = true;
             collider.transform.GetComponent<PlayerController>().CurrCannon = Cannon;
+        }
+        if (collider.gameObject.layer == 6)
+        {
+            movedown = false;
         }
     }
     private void OnTriggerExit2D(Collider2D collider)
@@ -18,6 +23,18 @@ public class CannonPlayerCheck : MonoBehaviour
             collider.transform.GetComponent<PlayerController>().TouchingCannon = false;
             collider.transform.GetComponent<PlayerController>().CurrCannon = null;
             collider.transform.GetComponent<PlayerController>().CanMove = true;
+        }
+
+        if (collider.gameObject.layer == 6)
+        {
+            movedown = true;
+        }
+    }
+    private void Update()
+    {
+        if (movedown)
+        {
+            this.transform.localPosition -= new Vector3(0, 0.01f, 0);
         }
     }
 }
