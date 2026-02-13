87718337dfa3951bfaaa63a2346c04c30c1f620e
GeorgeGimme <144726058+imasleeping@users.noreply.github.com>
2026-02-13 03:54:04 -0500

    Added points and win conditions

--- DW W26 Unity/Assets/PointsManager.cs
+++ DW W26 Unity/Assets/PointsManager.cs
@@ -1,12 +1,47 @@
+using Newtonsoft.Json.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PointsManager : MonoBehaviour
 {
     public int Team1Points;
     public int Team2Points;
-    // Update is called once per frame
-    void Update()
+    public Sprite Win;
+    public Sprite Lose;
+    public Sprite Draw;
+    public Sprite nothing;
+    public SpriteRenderer Screen1;
+    public SpriteRenderer Screen2;
+    public void Start()
     {
+        Screen1.sprite = nothing;
+        Screen2.sprite = nothing;
+    }
+    public void RoundEnd()
+    {
+        if (Team1Points > Team2Points)
+        {
+            Screen1.sprite = Win;
+            Screen2.sprite = Lose;
+        }
+        else if (Team2Points > Team1Points)
+        {
+            Screen1.sprite = Lose;
+            Screen2.sprite = Win;
+        }
+        else
+        {
+
+            Screen1.sprite = Draw;
+            Screen2.sprite = Draw;
+        }
+
+        Invoke("quit", 8f);
+    }
+
+    private void quit()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- DW W26 Unity/Assets/Scripts/WinTarget.cs
+++ DW W26 Unity/Assets/Scripts/WinTarget.cs
@@ -3,6 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class WinTarget : MonoBehaviour
 {
+    public GameObject points;
+    public bool Rightside;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +14,14 @@ public class WinTarget : MonoBehaviour
     // Update is called on bullet hit
     public void Hit(int damage)
     {
-        SceneManager.LoadScene(2);
+        if (Rightside)
+        {
+            points.transform.GetComponent<PointsManager>().Team1Points += 100;
+        }
+        else
+        {
+            points.transform.GetComponent<PointsManager>().Team2Points += 100;
+        }
+        points.transform.GetComponent<PointsManager>().RoundEnd();
     }
 }
